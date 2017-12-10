6f95398cea1d5afa8d1e7d8ec0d5773aaef78018
woitech <wojlow@gmail.com>
2017-12-10 16:08:52 +0100

    Class Crap changed

--- src/main/java/pl/plh/crap/Crap.java
+++ src/main/java/pl/plh/crap/Crap.java
@@ -2,6 +2,7 @@ package pl.plh.crap;
 
 public class Crap {
     public static void main(String[] args) {
-        System.out.println("Crap");
+        System.out.println("Crap changed");
+
     }
 }
