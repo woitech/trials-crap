ac4249a886e0a2e6f4beb313136a3d329629c1df
user <user@example.com>
2017-12-10 14:32:05 +0100

    First project

---
+++ README.md
@@ -0,0 +1,1 @@
+![car](https://ibb.co/cWvYqG)
---
+++ src/main/java/pl/plh/crap/Crap.java
@@ -0,0 +1,7 @@
+package pl.plh.crap;
+
+public class Crap {
+    public static void main(String[] args) {
+        System.out.println("Crap");
+    }
+}
