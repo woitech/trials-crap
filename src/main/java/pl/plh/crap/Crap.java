d06630f2b9b5d29950374e68fb910f402fd3268e
woitech <wojlow@gmail.com>
2017-12-17 22:48:06 +0100

    Chenged second time

--- src/main/java/pl/plh/crap/Crap.java
+++ src/main/java/pl/plh/crap/Crap.java
@@ -3,6 +3,7 @@ package pl.plh.crap;
 public class Crap {
     public static void main(String[] args) {
         System.out.println("Crap changed");
+        System.out.println("and changed");
 
     }
 }
